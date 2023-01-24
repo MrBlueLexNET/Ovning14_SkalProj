edbb585fb063e473e4f4ca01b7e089bd0e1c0a05
JYM <jeanyvesmichel@yahoo.com>
2023-01-24 15:30:55 +0100

    ApplicationContextFactory Added

---
+++ Ovning14_SkalProj.Data/ApplicationContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning14_SkalProj.Data
+{
+  
+        //public class ApplicationContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+        //{
+        //    public ApplicationDbContext CreateDbContext(string[] args)
+        //    {
+        //        var options = new DbContextOptionsBuilder<ApplicationDbContext>();
+        //        options.UseSqlServer("Not used here");
+
+        //        return new ApplicationDbContext(options.Options);
+
+        //    }
+        //} 
+}

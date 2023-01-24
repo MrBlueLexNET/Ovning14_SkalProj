30d10a563a7d8647529f4956d1145173f97502fc
JYM <jeanyvesmichel@yahoo.com>
2023-01-24 10:58:12 +0100

    Authorize Attribute test for HomeController

--- Ovning14_SkalProj/Controllers/HomeController.cs
+++ Ovning14_SkalProj/Controllers/HomeController.cs
@@ -1,16 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Ovning14_SkalProj.Data;
 using Ovning14_SkalProj.Models;
+using System.Data;
 using System.Diagnostics;
 
 namespace Ovning14_SkalProj.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly ApplicationDbContext db;
+        private readonly RoleManager<IdentityRole> roleManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager, ApplicationDbContext db, RoleManager<IdentityRole> roleManager)
         {
             _logger = logger;
+            this.userManager = userManager;
+            this.db = db;
+            this.roleManager = roleManager;
         }
 
         public IActionResult Index()

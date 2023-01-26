a497cd6fbe1119df9f85ae97662d8953ea2d9d2e
JYM <jeanyvesmichel@yahoo.com>
2023-01-26 14:01:20 +0100

    dbo.intructors[Data] Seed fr added

--- Ovning14_SkalProj.Data/Data/SeedData.cs
+++ Ovning14_SkalProj.Data/Data/SeedData.cs
@@ -1,6 +1,7 @@
 ﻿using Bogus;
 using Bogus.DataSets;
 using Microsoft.EntityFrameworkCore;
+using Ovning14_SkalProj.Core.Entities;
 
 namespace Ovning14_SkalProj.Data.Data
 {
@@ -12,11 +13,39 @@ namespace Ovning14_SkalProj.Data.Data
             //Guard Clause if something 
             if (await db.instructors.AnyAsync()) return;
 
-            faker = new Faker("sv");
+            faker = new Faker("fr");
 
             var intructors = GenerateInstructors(50);
             await db.AddRangeAsync(intructors);
+            await db.SaveChangesAsync();
 
+        }
+
+        private static IEnumerable<Instructor> GenerateInstructors(int numberOfInstructor)
+        {
+            var instructors = new List<Instructor>();
+            for (int i = 0; i < numberOfInstructor; i++) 
+            {
+                var fName = faker.Name.FirstName();
+                var lName = faker.Name.LastName();
+                var isPersonalTrainer = faker.Random.Bool();
+                var biography = faker.Lorem.Paragraph();
+                //var rowVersion = faker.Date.Timespan();
+
+                var instructor = new Instructor
+                {
+                    FirstName = fName,
+                    LastName = lName,
+                    IsPersonalTrainer = isPersonalTrainer,
+                    Biography = biography
+                    //RowVersion = rowVersion
+
+                };
+                instructors.Add(instructor);
+
+            }
+            return instructors;
+            
         }
     }
 }

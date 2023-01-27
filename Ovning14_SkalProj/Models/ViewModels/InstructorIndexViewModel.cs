6b8d18cc548ebe47793af361ae1ff31afc189db6
JYM <jeanyvesmichel@yahoo.com>
2023-01-27 09:34:24 +0100

    Layout cards in Bootstrapp container

---
+++ Ovning14_SkalProj/Models/ViewModels/InstructorIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Ovning14_SkalProj.Core.Entities;
+
+namespace Ovning14_SkalProj.Models.ViewModels
+{
+    public class InstructorIndexViewModel
+    {
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<GymClass> GymClasses { get; set; }
+        public IEnumerable<ApplicationUserGymClass> AppUsersGymClasses { get; set; }
+    }
+}

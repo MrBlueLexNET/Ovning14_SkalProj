19f1238e409bb2f563cfe1182129b528ecdd4573
JYM <jeanyvesmichel@yahoo.com>
2023-01-27 16:07:02 +0100

    InstructorCardViewComponent Added

---
+++ Ovning14_SkalProj/ViewComponents/InstructorCardViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ovning14_SkalProj.Core.Entities;
+
+namespace Ovning14_SkalProj.ViewComponents
+{
+    public class InstructorCardViewComponent : ViewComponent
+    {
+      
+        public IViewComponentResult Invoke(
+            Instructor instructor)
+        {
+            return View(instructor);
+        }
+    }
+
+}

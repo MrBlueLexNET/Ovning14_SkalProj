1e93c18844b3455e76944ae0a970a9248ec5e4bf
JYM <jeanyvesmichel@yahoo.com>
2023-01-28 09:35:27 +0100

    static class ApplicationBuilderExtensions Added

---
+++ Ovning14_SkalProj/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Ovning14_SkalProj.Data.Data;
+using Ovning14_SkalProj.Data;
+
+namespace Ovning14_SkalProj.Extensions
+{
+    public class ApplicationBuilderExtensions
+    {
+
+        public static async Task SeedDataAsync(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var db = serviceProvider.GetRequiredService<ApplicationDbContext>();
+
+                //db.Database.EnsureDeleted();
+                //db.Database.Migrate();
+
+                //dotnet user-secrets set "AdminPW" "BytMig123!"
+                var config = serviceProvider.GetRequiredService<IConfiguration>();
+                var adminPW = config["AdminPW"];
+
+                ArgumentNullException.ThrowIfNull(adminPW, nameof(adminPW));
+
+                try
+                {
+                    //await SeedData.InitAsync(db, serviceProvider, adminPW);
+                    await SeedData.InitAsync(db);
+                }
+                catch (Exception e)
+                {
+
+                    throw;
+                }
+            }
+        }
+    }
+}
+

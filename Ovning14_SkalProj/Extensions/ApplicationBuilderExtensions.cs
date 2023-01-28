71c02efb97c6d96fe102d52d6893a7bb8161f5ef
JYM <jeanyvesmichel@yahoo.com>
2023-01-28 09:38:43 +0100

    Simple using and catch to console added

--- Ovning14_SkalProj/Extensions/ApplicationBuilderExtensions.cs
+++ Ovning14_SkalProj/Extensions/ApplicationBuilderExtensions.cs
@@ -3,35 +3,33 @@ using Ovning14_SkalProj.Data;
 
 namespace Ovning14_SkalProj.Extensions
 {
-    public class ApplicationBuilderExtensions
+    public static class ApplicationBuilderExtensions
     {
 
         public static async Task SeedDataAsync(this IApplicationBuilder app)
         {
-            using (var scope = app.ApplicationServices.CreateScope())
+            using var scope = app.ApplicationServices.CreateScope();
+            var serviceProvider = scope.ServiceProvider;
+            var db = serviceProvider.GetRequiredService<ApplicationDbContext>();
+
+            //db.Database.EnsureDeleted();
+            //db.Database.Migrate();
+
+            //dotnet user-secrets set "AdminPW" "BytMig123!"
+            var config = serviceProvider.GetRequiredService<IConfiguration>();
+            var adminPW = config["AdminPW"];
+
+            ArgumentNullException.ThrowIfNull(adminPW, nameof(adminPW));
+
+            try
             {
-                var serviceProvider = scope.ServiceProvider;
-                var db = serviceProvider.GetRequiredService<ApplicationDbContext>();
-
-                //db.Database.EnsureDeleted();
-                //db.Database.Migrate();
-
-                //dotnet user-secrets set "AdminPW" "BytMig123!"
-                var config = serviceProvider.GetRequiredService<IConfiguration>();
-                var adminPW = config["AdminPW"];
-
-                ArgumentNullException.ThrowIfNull(adminPW, nameof(adminPW));
-
-                try
-                {
-                    //await SeedData.InitAsync(db, serviceProvider, adminPW);
-                    await SeedData.InitAsync(db);
-                }
-                catch (Exception e)
-                {
-
-                    throw;
-                }
+                //await SeedData.InitAsync(db, serviceProvider, adminPW);
+                await SeedData.InitAsync(db);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
             }
         }
     }
